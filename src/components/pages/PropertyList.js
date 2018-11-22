1efef828a3007f1745da708298767f8663304801
Pratik <pratik.mendapara@bacancytechnology.com>
2018-11-22 15:25:29 +0530

    product list page component and ui integration

---
+++ src/components/pages/PropertyList.js
@@ -0,0 +1,262 @@
+import React, { Component } from 'react';
+
+class PropertyList extends Component{
+	render () {
+		return (
+				<div className="PropertyList">
+					 <section className="top-head-area">
+				        <div className="container-fluid">
+				        	<div className="row top-nav-title">
+				            	<div className="col-md-3">
+				                	<a href="#" className="map-btn">Map</a>
+				            	</div>
+				                <div className="col-md-7">
+				                	<div className="search-area">
+				                        <label>Where do you serach like</label>
+				                        <form action="#" method="post" className="form-area">
+				                            <input type="text" name="name" value="Denver" className="form-control"/>
+				                            <button className="search-btn">Search</button> 
+				                        </form>
+				                    </div>
+				                </div>
+				                <div className="col-md-2">
+				                	<span className="km-text">2 KM</span>
+				                </div>
+				            </div>
+				        </div>    		   
+				    </section>
+				    
+				    <section className="contain-area">
+				        <div className="container-fluid">
+				        	<div className="row contain-plr">
+				                <div className="list-min-div">
+				                    <div className="estate-works-item">
+				                        <div className="item-overlay">
+				                        	<div className="book-category clip-path-right"><a href="javascript:;">Book Tour</a></div>
+				                        	<div className="category"><a href="javascript:;">Buy</a></div>
+				                        	<img className="category-img" src="images/portfolio_01.jpg" alt="" />
+				                            <div className="overlay-content">
+				                            	<h6 className="overlay-title">$8,800,000</h6>
+				                                <ul className="overlay-icons">
+				                                    <li className="mr-2">Standard</li>
+				                                    <li><a href="javascript:;"><i className="fa fa-star"></i></a></li>
+				                                    <li><a href="javascript:;"><i className="fa fa-star"></i></a></li>
+				                                    <li><a href="javascript:;"><i className="fa fa-star"></i></a></li>
+				                                    <li><a href="javascript:;"><i className="fa fa-star"></i></a></li>
+				                                    <li><a className="disable" href="javascript:;"><i className="fa fa-star"></i></a></li>
+				                                </ul>
+				                            </div>
+				                        </div>
+				                        <div className="estate-works-description">
+				                            <div className="estate-details">
+				                            	<h4 className="d-flex">
+				                                	<span>Mega Villa</span>
+				                                	<a href="#" className="btn small-btn">Video Visit</a>
+				                                </h4>
+				                                <strong>ABC Street 1,12586 Denver</strong>
+				                                <p>Will equipped studio apartment From</p>
+				                                <p>2.00 QM | 25 Bedrooms</p>
+				                            </div>
+				                            <ul className="estate-bottom-nav">
+				                                <li><a href="javascript:;"><i className="fa fa-heart"></i> Save</a></li>
+				                                <li><a href="javascript:;"><i className="fa fa-phone"></i> Contact</a></li>
+				                                <li><a href="javascript:;"><i className="fa fa-handshake-o"></i> Apply</a></li>
+				                                <li><a href="javascript:;"><i className="fa fa-share-alt"></i> Share</a></li>
+				                            </ul>
+				                        </div>
+				                    </div>
+				                </div>
+				                <div className="list-min-div">
+				                    <div className="estate-works-item">
+				                        <div className="item-overlay">
+				                        	<div className="book-category clip-path-right"><a href="javascript:;">Book Tour</a></div>
+				                        	<div className="category"><a href="javascript:;">Buy</a></div>
+				                        	<img className="category-img" src="images/portfolio_01.jpg" alt="" />
+				                            <div className="overlay-content">
+				                            	<h6 className="overlay-title">$8,800,000</h6>
+				                                <ul className="overlay-icons">
+				                                    <li className="mr-2">Standard</li>
+				                                    <li><a href="javascript:;"><i className="fa fa-star"></i></a></li>
+				                                    <li><a href="javascript:;"><i className="fa fa-star"></i></a></li>
+				                                    <li><a href="javascript:;"><i className="fa fa-star"></i></a></li>
+				                                    <li><a href="javascript:;"><i className="fa fa-star"></i></a></li>
+				                                    <li><a className="disable" href="javascript:;"><i className="fa fa-star"></i></a></li>
+				                                </ul>
+				                            </div>
+				                        </div>
+				                        <div className="estate-works-description">
+				                            <div className="estate-details">
+				                            	<h4 className="d-flex">
+				                                	<span>Mega Villa</span>
+				                                	<a href="#" className="btn small-btn">Video Visit</a>
+				                                </h4>
+				                                <strong>ABC Street 1,12586 Denver</strong>
+				                                <p>Will equipped studio apartment From</p>
+				                                <p>2.00 QM | 25 Bedrooms</p>
+				                            </div>
+				                            <ul className="estate-bottom-nav">
+				                                <li><a href="javascript:;"><i className="fa fa-heart"></i> Save</a></li>
+				                                <li><a href="javascript:;"><i className="fa fa-phone"></i> Contact</a></li>
+				                                <li><a href="javascript:;"><i className="fa fa-handshake-o"></i> Apply</a></li>
+				                                <li><a href="javascript:;"><i className="fa fa-share-alt"></i> Share</a></li>
+				                            </ul>
+				                        </div>
+				                    </div>
+				                </div>
+				                <div className="list-min-div">
+				                    <div className="estate-works-item">
+				                        <div className="item-overlay">
+				                        	<div className="book-category clip-path-right"><a href="javascript:;">Book Tour</a></div>
+				                        	<div className="category rent-categ"><a href="javascript:;">Rent</a></div>
+				                        	<img className="category-img" src="images/portfolio_01.jpg" alt="" />
+				                            <div className="overlay-content">
+				                            	<h6 className="overlay-title">$8,800,000</h6>
+				                                <ul className="overlay-icons">
+				                                    <li className="mr-2">Standard</li>
+				                                    <li><a href="javascript:;"><i className="fa fa-star"></i></a></li>
+				                                    <li><a href="javascript:;"><i className="fa fa-star"></i></a></li>
+				                                    <li><a href="javascript:;"><i className="fa fa-star"></i></a></li>
+				                                    <li><a href="javascript:;"><i className="fa fa-star"></i></a></li>
+				                                    <li><a className="disable" href="javascript:;"><i className="fa fa-star"></i></a></li>
+				                                </ul>
+				                            </div>
+				                        </div>
+				                        <div className="estate-works-description">
+				                            <div className="estate-details">
+				                            	<h4 className="d-flex">
+				                                	<span>Mega Villa</span>
+				                                	<a href="#" className="btn small-btn">Video Visit</a>
+				                                </h4>
+				                                <strong>ABC Street 1,12586 Denver</strong>
+				                                <p>Will equipped studio apartment From</p>
+				                                <p>2.00 QM | 25 Bedrooms</p>
+				                            </div>
+				                            <ul className="estate-bottom-nav">
+				                                <li><a href="javascript:;"><i className="fa fa-heart"></i> Save</a></li>
+				                                <li><a href="javascript:;"><i className="fa fa-phone"></i> Contact</a></li>
+				                                <li><a href="javascript:;"><i className="fa fa-handshake-o"></i> Apply</a></li>
+				                                <li><a href="javascript:;"><i className="fa fa-share-alt"></i> Share</a></li>
+				                            </ul>
+				                        </div>
+				                    </div>
+				                </div>
+				                <div className="list-min-div">
+				                    <div className="estate-works-item">
+				                        <div className="item-overlay">
+				                        	<div className="book-category clip-path-right"><a href="javascript:;">Book Tour</a></div>
+				                        	<div className="category"><a href="javascript:;">Buy</a></div>
+				                        	<img className="category-img" src="images/portfolio_01.jpg" alt="" />
+				                            <div className="overlay-content">
+				                            	<h6 className="overlay-title">$8,800,000</h6>
+				                                <ul className="overlay-icons">
+				                                    <li className="mr-2">Standard</li>
+				                                    <li><a href="javascript:;"><i className="fa fa-star"></i></a></li>
+				                                    <li><a href="javascript:;"><i className="fa fa-star"></i></a></li>
+				                                    <li><a href="javascript:;"><i className="fa fa-star"></i></a></li>
+				                                    <li><a href="javascript:;"><i className="fa fa-star"></i></a></li>
+				                                    <li><a className="disable" href="javascript:;"><i className="fa fa-star"></i></a></li>
+				                                </ul>
+				                            </div>
+				                        </div>
+				                        <div className="estate-works-description">
+				                            <div className="estate-details">
+				                            	<h4 className="d-flex">
+				                                	<span>Mega Villa</span>
+				                                	<a href="#" className="btn small-btn">Video Visit</a>
+				                                </h4>
+				                                <strong>ABC Street 1,12586 Denver</strong>
+				                                <p>Will equipped studio apartment From</p>
+				                                <p>2.00 QM | 25 Bedrooms</p>
+				                            </div>
+				                            <ul className="estate-bottom-nav">
+				                                <li><a href="javascript:;"><i className="fa fa-heart"></i> Save</a></li>
+				                                <li><a href="javascript:;"><i className="fa fa-phone"></i> Contact</a></li>
+				                                <li><a href="javascript:;"><i className="fa fa-handshake-o"></i> Apply</a></li>
+				                                <li><a href="javascript:;"><i className="fa fa-share-alt"></i> Share</a></li>
+				                            </ul>
+				                        </div>
+				                    </div>
+				                </div>
+				                <div className="list-min-div">
+				                    <div className="estate-works-item">
+				                        <div className="item-overlay">
+				                        	<div className="book-category clip-path-right"><a href="javascript:;">Book Tour</a></div>
+				                        	<div className="category rent-categ"><a href="javascript:;">Rent</a></div>
+				                        	<img className="category-img" src="images/portfolio_01.jpg" alt="" />
+				                            <div className="overlay-content">
+				                            	<h6 className="overlay-title">$8,800,000</h6>
+				                                <ul className="overlay-icons">
+				                                    <li className="mr-2">Standard</li>
+				                                    <li><a href="javascript:;"><i className="fa fa-star"></i></a></li>
+				                                    <li><a href="javascript:;"><i className="fa fa-star"></i></a></li>
+				                                    <li><a href="javascript:;"><i className="fa fa-star"></i></a></li>
+				                                    <li><a href="javascript:;"><i className="fa fa-star"></i></a></li>
+				                                    <li><a className="disable" href="javascript:;"><i className="fa fa-star"></i></a></li>
+				                                </ul>
+				                            </div>
+				                        </div>
+				                        <div className="estate-works-description">
+				                            <div className="estate-details">
+				                            	<h4 className="d-flex">
+				                                	<span>Mega Villa</span>
+				                                	<a href="#" className="btn small-btn">Video Visit</a>
+				                                </h4>
+				                                <strong>ABC Street 1,12586 Denver</strong>
+				                                <p>Will equipped studio apartment From</p>
+				                                <p>2.00 QM | 25 Bedrooms</p>
+				                            </div>
+				                            <ul className="estate-bottom-nav">
+				                                <li><a href="javascript:;"><i className="fa fa-heart"></i> Save</a></li>
+				                                <li><a href="javascript:;"><i className="fa fa-phone"></i> Contact</a></li>
+				                                <li><a href="javascript:;"><i className="fa fa-handshake-o"></i> Apply</a></li>
+				                                <li><a href="javascript:;"><i className="fa fa-share-alt"></i> Share</a></li>
+				                            </ul>
+				                        </div>
+				                    </div>
+				                </div>
+				                <div className="list-min-div">
+				                    <div className="estate-works-item">
+				                        <div className="item-overlay">
+				                        	<div className="book-category clip-path-right"><a href="javascript:;">Book Tour</a></div>
+				                        	<div className="category"><a href="javascript:;">Buy</a></div>
+				                        	<img className="category-img" src="images/portfolio_01.jpg" alt="" />
+				                            <div className="overlay-content">
+				                            	<h6 className="overlay-title">$8,800,000</h6>
+				                                <ul className="overlay-icons">
+				                                    <li className="mr-2">Standard</li>
+				                                    <li><a href="javascript:;"><i className="fa fa-star"></i></a></li>
+				                                    <li><a href="javascript:;"><i className="fa fa-star"></i></a></li>
+				                                    <li><a href="javascript:;"><i className="fa fa-star"></i></a></li>
+				                                    <li><a href="javascript:;"><i className="fa fa-star"></i></a></li>
+				                                    <li><a className="disable" href="javascript:;"><i className="fa fa-star"></i></a></li>
+				                                </ul>
+				                            </div>
+				                        </div>
+				                        <div className="estate-works-description">
+				                            <div className="estate-details">
+				                            	<h4 className="d-flex">
+				                                	<span>Mega Villa</span>
+				                                	<a href="#" className="btn small-btn">Video Visit</a>
+				                                </h4>
+				                                <strong>ABC Street 1,12586 Denver</strong>
+				                                <p>Will equipped studio apartment From</p>
+				                                <p>2.00 QM | 25 Bedrooms</p>
+				                            </div>
+				                            <ul className="estate-bottom-nav">
+				                                <li><a href="javascript:;"><i className="fa fa-heart"></i> Save</a></li>
+				                                <li><a href="javascript:;"><i className="fa fa-phone"></i> Contact</a></li>
+				                                <li><a href="javascript:;"><i className="fa fa-handshake-o"></i> Apply</a></li>
+				                                <li><a href="javascript:;"><i className="fa fa-share-alt"></i> Share</a></li>
+				                            </ul>
+				                        </div>
+				                    </div>
+				                </div>
+				        	</div>
+				        </div>    		   
+				    </section>
+				 </div>
+		);
+	}
+}
+
+export default PropertyList;
--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,14 @@ import { Redirect, Switch, Route } from 'react-router-dom';
 
 import Layout from './components/Layout';
 import Home from './components/pages/Home';
+import PropertyList from './components/pages/PropertyList';
 import Loader from './components/common/Loader';
 
 export default () => (
 		<Layout>
 			<Switch>
 				<Route exact path='/' component={Home} />
+				<Route exact path='/propert-list' component={PropertyList} />
 				<Route exact path='/loader' component={Loader} />
 			</Switch>
 		</Layout>
