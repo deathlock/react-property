4c3bbc25e8bacf606fb22fd14ba600f0a745620e
Smit27 <smith29raval@gmail.com>
2018-12-05 17:43:39 +0530

    Merged changes

--- src/redux/actions/Sync.action.js
+++ src/redux/actions/Sync.action.js
@@ -3,12 +3,8 @@ export const searchTermData = payload => ({ type: 'SEARCH_TERM', payload });
 export const setPlaceId = payload => ({ type: 'SET_PLACE_ID', payload });
 
 /***** Property Listing */
-<<<<<<< Updated upstream
 export const propertyListData = payload => ({ type: 'PROPERTY_LIST', payload });
 
 /**** User Token */
 export const userLoggedIn = payload => ({type: 'SET_LOGGEDIN_STATE', payload});
 export const userTokenData = payload => ({ type: 'USER_TOKEN', payload });
-=======
-export const propertyListData = payload => ({ type: 'PROPERTY_LIST', payload });
->>>>>>> Stashed changes
